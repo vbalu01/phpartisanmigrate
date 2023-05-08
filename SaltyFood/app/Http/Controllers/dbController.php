7e6be0b5472e96ce242e162c8a34bb8e151ace94
KiKiHUN <49228599+KiKiHUN@users.noreply.github.com>
2023-05-08 22:31:33 +0200

    balu_edition

--- SaltyFood/app/Http/Controllers/dbController.php
+++ SaltyFood/app/Http/Controllers/dbController.php
@@ -10,7 +10,7 @@ class dbController extends Controller
 {
     public function teszt()
     {
-        
+        return csrf_token();
     }
 
     public function getAllRestaurants()
@@ -41,5 +41,43 @@ class dbController extends Controller
     {
         $tmp = DB::table('foods')->select(['id', 'r_id', 'f_name', 'c_id', 'description', 'price'])->where([['available', '=', true]])->get();
         return View('tesco', ['data' => $tmp]);
+    }
+    public function addNewFood(Request $request){
+        if($request->has('r_id') && $request->has('f_name') && ($request->has('c_id') && is_numeric($request->input('c_id'))) && $request->has('description') && ($request->has('price') && is_numeric($request->input('price')))){ //Beérkező adatok meglétének ellenőrzése
+            //Ha admin, vagy adott étterem ellenőrzés, ha admin +étterem létezés ellenőrzés
+            if((int)$request->input('price') < 0){//Még az étterem fizet hogy náluk egyél? Olyan nekem is kéne
+                //...
+                return 'Én is kérek ingyen kaját';
+            }else{
+                //Kategória létezésének ellenőrzése
+                DB::table('foods')->insert([
+                    'r_id' => $request->input('r_id'),
+                    'f_name' => $request->input('f_name'),
+                    'c_id' => $request->input('c_id'),
+                    'description' => $request->input('description'),
+                    'price' => (int)$request->input('price'),
+                    'available' => true
+                ]);
+                return 'Asszem sikerült';
+            }
+        }else{ //Nem érkezett meg minden adat
+            return 'Elbasztad';
+        }
+    }
+
+    public function addNewCategory(Request $request){
+        if($request->has('c_name')){ //Beérkező OK
+
+        }else{ //Hiányos adatok
+
+        }
+    }
+
+    public function addNewAddress(Request $request){
+        if($request->has('u_id') && $request->has('a_name') && $request->has('city_id') && $request->has('address') && $request->has('phone')){//Adat ellenőrzés, other opcionális
+
+        }else{//Nem érkezett meg minden adat
+
+        }
     }
 }
