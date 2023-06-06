7c43e95e6e5d064eda07aebf16ec21835dee7e67
KiKiHUN <49228599+KiKiHUN@users.noreply.github.com>
2023-06-06 23:40:49 +0200

    UI fix

--- SaltyFood/public/js/main.js
+++ SaltyFood/public/js/main.js
@@ -202,23 +202,17 @@
     /*-------------------
 		Quantity change
 	--------------------- */
-    var proQty = $('.pro-qty');
-    proQty.prepend('<span class="dec qtybtn">-</span>');
-    proQty.append('<span class="inc qtybtn">+</span>');
-    proQty.on('click', '.qtybtn', function () {
-        var $button = $(this);
-        var oldValue = $button.parent().find('input').val();
-        if ($button.hasClass('inc')) {
-            var newVal = parseFloat(oldValue) + 1;
-        } else {
-            // Don't allow decrementing below zero
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
-        }
-        $button.parent().find('input').val(newVal);
-    });
+
 
 })(jQuery);
+function getNumbers(inputString){
+    var regex=/\d+\.\d+|\.\d+|\d+/g,
+        results = [],
+        n;
+
+    while(n = regex.exec(inputString)) {
+        results.push(parseFloat(n[0]));
+    }
+
+    return results;
+}
